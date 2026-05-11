d6621bb322663012a70ff84345ce5df5d3eb651e
github.com/GarionMorgan <garmorgan14@yahoo.com>
2026-05-10 20:06:54 -0500

    Added backbone classes for game, player, and monster

--- monster_wrangler.py
+++ monster_wrangler.py
@@ -14,6 +14,71 @@ pygame.display.set_caption("Monster Wrangler")
 FPS = 60
 clock = pygame.time.Clock()
 
+#Define Classes
+class Game():
+    """A class to control game play"""
+    def __init__(self):
+        """Initialize game object"""
+        pass
+
+    def update(self):
+        """update game object"""
+        pass
+
+    def draw(self):
+        """draw objects to display"""
+        pass
+
+    def check_collisions(self):
+        """check for collisions between player and monsters"""
+        pass
+
+    def start_new_round(self):
+        """populate board with new monsters"""
+        pass
+
+    def choose_new_target(self):
+        """choose new target for player"""
+        pass
+
+    def pause_game(self):
+        """pause game"""
+        pass
+
+    def reset_game(self):
+        """reset game"""
+        pass
+
+class Player(pygame.sprite.Sprite):
+    """player class that user can control"""
+    def __init__(self):
+        """initialize player"""
+        pass
+
+    def update(self):
+        """update player"""
+        pass
+
+    def warp(self):
+        """warp player to safe zone"""
+        pass
+
+    def reset(self):
+        """resets player position"""
+        pass
+
+class Monster(pygame.sprite.Sprite):
+    """class to create enemy objects"""
+    def __init__(self):
+        """initialize monster"""
+        pass
+
+    def update(self):
+        """update monster"""
+        pass
+
+    
+
 #main game loop
 running = True
 while running:
